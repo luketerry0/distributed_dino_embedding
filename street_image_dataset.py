90474a46a4ffc34252d1acd017fb4b3f54f0ee85
Luke Terry <luke.h.terry-1@ou.edu>
2024-10-03 13:32:50 -0500

    fix bug in dataset

--- street_image_dataset.py
+++ street_image_dataset.py
@@ -3,6 +3,10 @@ import os
 from PIL import Image
 import pandas as pd
 
+from torchvision.transforms import v2
+import torchvision.transforms as transforms
+import torchvision
+
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
@@ -16,8 +20,8 @@ class StreetImageDataset(Dataset):
 
         length = 0
         for root, dirs, files in os.walk(imageRootDir):
-            #pics = [x for x in files if self.image_filter(x)]
-            length += len(files)
+            pics = [x for x in files if self.image_filter(x)]
+            length += len(pics)
 
         self.imageRootDir = imageRootDir
         self.transform = transform
@@ -29,11 +33,11 @@ class StreetImageDataset(Dataset):
     def __getitem__(self, idx):
         # return a specific image from the filesystem
         for root, dirs, files in os.walk(self.imageRootDir):
-            # pics = [x for x in files if self.image_filter(x)]
-            idx -= len(files)
-            if (idx <= 0):
-                idx += len(files)
-                file = root + '/' + files[idx]
+            pics = [x for x in files if self.image_filter(x)]
+            idx -= len(pics)
+            if (idx < 0):
+                idx += len(pics)
+                file = root + '/' + pics[idx]
                 break
         
         # read in the file as a PIL image
@@ -41,3 +45,24 @@ class StreetImageDataset(Dataset):
         if self.transform:
             image = self.transform(image)
         return image, file
+
+if __name__ == "__main__":
+
+    transform = v2.Compose(
+                    [
+                        torchvision.transforms.ToTensor(),
+                        transforms.Resize(size=(256, 256), antialias=True),
+                        transforms.CenterCrop((224, 224)),
+                        transforms.Normalize(
+                                            mean=[0.485, 0.456, 0.406],
+                                            std=[0.229, 0.224, 0.225]
+                                            ),
+                    ]
+                    )
+
+    dataset = StreetImageDataset("./test_bronx", transform=transform)
+    data = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=1)
+
+
+    for image, file in data:
+        print(file)
